1192e71ae0da227c799d31796de339c7a1b2e1e1
Adam Menicucci <adam.menicucci@salesianum.org>
2023-09-02 20:06:22 -0400

    fixed function input, added rounding

--- TemperatureConverter.java
+++ TemperatureConverter.java
@@ -4,26 +4,38 @@
 // Input, basic math operations, and conditional statements
 
 import java.util.Scanner;
+import java.text.DecimalFormat;
 
 public class TemperatureConverter {
     
-    public static double FtoC(double cels) {
+    public static double CtoFround(double degrees) {
         double a = 1.8;
         double b = 32.0;
-        double degrees = nextDouble();
-        return (a * degrees) + b;
+        double fahrenheit = (a * degrees) + b;
+        return Round(fahrenheit);
     }
 
-    private static double nextDouble() {
-        return 0;
-    }
-
-    public static double CtoF(double fahr) {
+    public static double FtoCround(double degrees) {
         double c = 9.0;
         double d = 5.0;
         double e = 32.0;
-        double degrees = nextDouble();
-        return (degrees - e) * (c/d);
+        double celsius = (degrees - e) * (d/c);
+        return Round(celsius);
+    }
+
+    public static double CtoF(double degrees) {
+        double a = 1.8;
+        double b = 32.0;
+        double fahrenheit = (a * degrees) + b;
+        return fahrenheit;
+    }
+
+    public static double FtoC(double degrees) {
+        double c = 9.0;
+        double d = 5.0;
+        double e = 32.0;
+        double celsius = (degrees - e) * (d/c);
+        return celsius;
     }
 
     public static void main(String[] args) {
@@ -32,31 +44,67 @@ public class TemperatureConverter {
 
         //Welcome messages, request for input
         System.out.println("Welcome to the Temperature Converter!");
-        System.out.print("What unit are you starting with?");
-        String unit = scanner.nextLine();
-        System.out.print("How many degrees °" + unit + " are there?");
+        System.out.print("What unit are you starting with? ");
+        String unit = scanner.nextLine().toUpperCase();
+        System.out.print("How many degrees " + unit + " are there? ");
         double degrees = scanner.nextDouble();
+        System.out.print("Would you like your conversion to be rounded? (Yes/No) ");
+        String answer = scanner.next().toUpperCase();
+        System.out.println("Doing the conversion...");
 
-        
+        if (answer.equals("YES")) {
+            if (unit.equals("C") || unit.equals("CELSIUS")) {
+            double rfahrenheit = CtoFround(degrees);
+            System.out.print(degrees + "°C in Fahrenheit is about " + rfahrenheit + "°F.");
+            } else if (unit.equals("F") || unit.equals("FAHRENHEIT")) {
+                double rcelsius = FtoCround(degrees);
+                System.out.print(degrees + "°F in Celsius is about " + rcelsius + "°C.");
+            } else {
+                System.out.println("That unit does not exist.");
+                System.out.println("Make sure the unit is capitalized for the program to work.");
 
-        if (unit.equals("C") || unit.equals("Celsius")) {
-            System.out.print(degrees + "°C in Fahrenheit is " + cels + "°F.");
-        } else if (unit.equals("F") || unit.equals("Fahrenheit")) {
-            System.out.print(degrees + "°F in Celsius is " + fahr + "°C.");
-        } else {
-            System.out.println("That unit does not exist.");
-            System.out.println("Make sure the unit is capitalized for the program to work.");
+                String restart;
 
-            String restart;
-            do {
-                // Your program logic here
+                do {
+                    System.out.print("Do you want to restart the program? (yes/no): ");
+                    restart = scanner.nextLine();
+                } while (restart.equalsIgnoreCase("yes"));
     
-                System.out.print("Do you want to restart the program? (yes/no): ");
-                restart = scanner.nextLine();
-            } while (restart.equalsIgnoreCase("yes"));
-    
-            System.out.println("Program has ended.");
-            scanner.close();
+                System.out.println("Program has ended.");
+            
         }
+        } else if (answer.equals("NO")) {
+            if (unit.equals("C") || unit.equals("CELSIUS")) {
+                double fahrenheit = CtoF(degrees);
+                System.out.print(degrees + "°C in Fahrenheit is about " + fahrenheit + "°F.");
+            } else if (unit.equals("F") || unit.equals("FAHRENHEIT")) {
+                double celsius = FtoC(degrees);
+                System.out.print(degrees + "°F in Celsius is about " + celsius + "°C.");
+            } else {
+                System.out.println("That unit does not exist.");
+                System.out.println("Make sure the unit is capitalized for the program to work.");
+
+                String restart;
+
+                do {
+                    System.out.print("Do you want to restart the program? (yes/no): ");
+                    restart = scanner.nextLine();
+                } while (restart.equalsIgnoreCase("yes"));
+    
+                System.out.println("Program has ended.");
+            
+        }
+        } else {
+            System.out.print("If you can't answer a yes or no question, we won't answer yours either.");
+        }
+        
+        scanner.close();
+    
     }
+
+    public static double Round(double value) {
+        DecimalFormat x = new DecimalFormat("#.#");
+        return Double.parseDouble(x.format(value));
+    }
+
 }
